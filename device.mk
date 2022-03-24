5d1135b84c95d8334a652755f070f9e878fafcbd
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    dew: Inherit launch_with_vendor_ramdisk for vAB
    - mt6768 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Inherit launch_with_vendor_ramdisk product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 35
 
