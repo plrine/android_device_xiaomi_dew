b2e683e4e83c18c64c1b87520f9d3c17866183dd
rio004 <riyon336@gmail.com>
2023-09-27 16:17:32 +0100

    dew: Import rootdir from stock
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4fd4b4e3564f61608928fb1faa48271f6c5ec4b1
    Signed-off-by: rio004 <riyon336@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.batterysecret.rc \
+    init.connectivity.rc \
+    init_connectivity.rc \
+    init.connectivity.common.rc \
+    init.insmod.sh \
+    init.mi_thermald.rc \
+    init.modem.rc \
+    init.mt6768.rc \
+    init.mt6768.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    ueventd.mtk.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 35
 
