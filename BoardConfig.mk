86554ef94c0cb3f8c129799ebbc497956d541aad
Michael Bestas <mkbestas@lineageos.org>
2024-02-28 22:45:13 +0000

    dew: Inherit common partition reserved sizes
    Change-Id: I444ce2fcd37cdfa58d83a7e872dc547fb05a2a89
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,8 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
 # Platform
 TARGET_BOARD_PLATFORM := mt6768
 
