ead1e7f46e409e7b054453b5ebaab4540d87da42
claxten10 <claxten10@gmail.com>
2025-04-20 22:21:04 +0200

    dew: Configure partitions
    Change-Id: Iaacccf1ef0e39f520c9910e972f40262b695ed4d
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -11,5 +11,8 @@ PRODUCT_SHIPPING_API_LEVEL := 35
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/dew/dew-vendor.mk)
