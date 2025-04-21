b1c7c3b031dc10bcf7c0ea17fa0d8cedde5351d4
claxten10 <claxten10@gmail.com>
2025-04-21 13:05:00 +0200

    dew: Add `hardware/mediatek` to soong namespaces
    Change-Id: I4cfa74d0e0524d9373af5bc37084c977b436743b
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ PRODUCT_SHIPPING_API_LEVEL := 35
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
