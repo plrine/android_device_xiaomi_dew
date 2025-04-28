d6cde54b450c4e4a12ead7565ccc8d155c7b54f3
claxten10 <claxten10@gmail.com>
2025-04-29 01:17:59 +0200

    dew: Configure WiFi stack
    Change-Id: I4a88421569bca5bc3009ae50d7504b833f842ecb
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -188,5 +188,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi-service
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi) \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/dew/dew-vendor.mk)
