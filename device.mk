2cc9481e0e9ccf4b02b860786fb3c8471a69f9a9
claxten10 <claxten10@gmail.com>
2025-05-01 19:02:49 +0200

    dew: Configure Fingerprint stack
    * Permissions from stock fingerprint hal init.
    
    Change-Id: Ibf948371fde8ea3274ce4767984a2a340b639316
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -143,6 +143,16 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.xiaomi
+
+PRODUCT_PACKAGES += \
+    init.mt6768.fingerprint.rc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # fstab
 PRODUCT_PACKAGES += \
     fstab.mt6768 \
