fd03d20498424ffef8d3ebbbf0a4647bccd02006
claxten10 <claxten10@gmail.com>
2025-07-02 06:40:30 +0000

    dew: Include common MediaTek/Xiaomi FCM
    Change-Id: I370cf53f9ca1cd4f77ca1e26cf927fd1b62f5701
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,9 @@ include device/mediatek/sepolicy_vndr/SEPolicy.mk
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # WiFi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
