2392b37a9a0240ad5f055a8ada10006a2982100d
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-05-02 18:12:40 +0700

    fixup a
    Change-Id: Ic6e486df858190c6cac9e8c9f195f186ca2392aa

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libcam.utils.sensorprovider.so',
     ): blob_fixup()
         .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+    'vendor/etc/init/android.hardware.media.c2-mediatek.rc': blob_fixup()
+        .regex_replace('c2-mediatek\n', 'c2-mediatek-64b\n'),
+    'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
