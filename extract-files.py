5504f4a0a23df1e4090409ad8223fefcfd9a660b
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-05-02 16:35:40 +0700

    mbut
    Change-Id: I0991cdcd4cd33dc393f95152d537885a81a3d8c6

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -16,9 +21,20 @@ namespace_imports = [
     'hardware/xiaomi',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/bin/mnld',
+        'vendor/lib64/libsensorndkbridge.so',
+        'vendor/lib64/libaalservice.so',
+        'vendor/lib64/libcam.utils.sensorprovider.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'dew',
     'xiaomi',
+    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
 )
 
